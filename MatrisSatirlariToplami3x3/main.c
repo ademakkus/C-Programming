7a4064bc0a0c2c4fdfcc0cafd584a06acb52b733
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-05-17 19:41:43 +0300

    Add files via upload

---
+++ MatrisSatirlariToplami3x3/main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void satirTopla(int[][3]);
+void yaz(int[][3]);
+int main(){
+int m1[3][3];
+int i,j;
+printf("Matrisin elemanlarini giriniz:\n");
+for(i=0;i<=2;i++)
+{
+for(j=0;j<=2;j++)
+scanf("%d", &m1[i][j]);
+}
+satirTopla(m1);
+return 0;
+}
+void yaz(int a[][3])
+{
+int i,j;
+for(i=0;i<=2;i++)
+{
+for(j=0;j<=2;j++)
+{
+printf(" %d ", a[i][j]);
+}
+printf("\n");}
+}
+void satirTopla(int a[][3])
+{
+int i,j;
+int toplam=0;
+printf("\n");
+yaz(a);
+printf("\n");
+printf("SONUC:\n\n");
+//Sonuç yazdýrýlýyor
+for(i=0;i<=2;i++)
+{
+for(j=0;j<=2;j++)
+{
+toplam+=a[i][j];
+}
+printf("%d. satir toplami %d \n",i+1 ,toplam);
+toplam=0;
+}
+}
