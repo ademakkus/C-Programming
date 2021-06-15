cd83c70668eb6e096fc55b7a57aad70db659b07a
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-06-15 10:49:32 +0300

    Add files via upload

---
+++ FactorialRecursive/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace FactorialRecursive
+{
+  class Program
+  {
+    static int Factorial(int number)
+    {
+      if (number<0)
+        return -1;
+    else  if (number == 0)
+        return 1;
+      return number * Factorial(number - 1);
+    }
+    static void Main(string[] args)
+    {
+      try
+      {
+        Console.Write("Enter the number to be factored in:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (Factorial(number) == -1)
+          Console.WriteLine("The factorial of the number {0} incalculable", number);
+        else
+        {
+          Console.WriteLine("The factorial of the number {0} you entered:{1}", number, Factorial(number));
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message.ToString());
+        
+      }
+      Console.ReadKey();
+    }
+  }
+}
---
+++ TenBaseTo2Base/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenBaseTo2Base
+{
+  class Program
+  {
+    static void WriteBit(int number)
+    {
+      if (number == 0)
+        return;
+     WriteBit(number >> 1);
+     
+      Console.Write(number & 1);
+
+    }
+    static void Main(string[] args)
+    {
+
+      Console.Write("Enter a number in base 10 :");
+      int number = Convert.ToInt32(Console.ReadLine()); 
+      Console.Write("In 10 base {0}",number);
+      Console.Write(" and in 2 base :");
+      WriteBit(number);    
+      Console.WriteLine("\nCommand line entered");
+      foreach (string item in args)
+      {
+        Console.WriteLine(item);
+      }
+      Console.ReadKey();
+    }
+  }
+}
