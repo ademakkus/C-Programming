b106947c8bd75ba987040e92242eb1ccc11a77e9
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-06-15 16:24:25 +0300

    Add files via upload

---
+++ CopyConstructor/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+  class Person
+  {
+    public Person()
+    {
+
+    }
+    public Person(string name, string surname, string departmant)
+    {
+      Name = name;
+      Surname = surname;
+      Departmant = departmant;
+    }
+    public Person(Person person)
+    {
+      this.Name = person.Name;
+      this.Surname = person.Surname;
+      this.Departmant = person.Departmant;
+    }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Departmant { get; set; }
+public void PrintInfo()
+  {
+    Console.WriteLine($"Name:{Name}\nSurname: {Surname} \nDepartmant: {Departmant}");
+  }
+  }
+  
+}
---
+++ CopyConstructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person person = new Person();
+      person.Name = "Adem";
+      person.Surname = "AKKUŞ";
+      person.Departmant = "Software Development";
+      person.PrintInfo();
+
+      Person personCopy = new Person(person);
+
+      personCopy.PrintInfo();
+      Console.ReadKey();
+    }
+  }
+}
